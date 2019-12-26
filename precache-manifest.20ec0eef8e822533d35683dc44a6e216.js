d949b4374405017e76ddf1b4d269903545a2617d
Mark Vidad <mvidad@pelmorex.com>
2019-12-26 15:24:01 -0500

    Updates

---
+++ precache-manifest.20ec0eef8e822533d35683dc44a6e216.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9aba030e40b9f389b6b06c17fa43db9d",
+    "url": "/crwn-clothing/index.html"
+  },
+  {
+    "revision": "6de4888f1d27e1a37209",
+    "url": "/crwn-clothing/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "d531e63c232f21746e16",
+    "url": "/crwn-clothing/static/js/2.8101ec07.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/crwn-clothing/static/js/2.8101ec07.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6de4888f1d27e1a37209",
+    "url": "/crwn-clothing/static/js/main.efef5dfe.chunk.js"
+  },
+  {
+    "revision": "8e1f379f0858b5b017bc",
+    "url": "/crwn-clothing/static/js/runtime-main.01128f07.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/crwn-clothing/static/media/logo.5d5d9eef.svg"
+  }
+]);
