d81fb608d2393c97eb3193e63e32bafbc708787a
Mark Vidad <mvidad@pelmorex.com>
2019-12-26 18:47:11 -0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/crwn-clothing/precache-manifest.9f60e1380f309e4693d21417ea09f537.js"
+  "/crwn-clothing/precache-manifest.476fcd27dc9cce4b62cf69d094d50335.js"
 );
 
 self.addEventListener('message', (event) => {
