08f8769982bedb61cc67324077a3e9142a16642e
Mark Vidad <markvillavidad@yahoo.com>
2019-12-28 15:56:19 -0500

    Added header component

--- src/App.js
+++ src/App.js
@@ -1,27 +1,19 @@
 import React from 'react';
 import './App.css';
-import {Switch, Route, Link} from 'react-router-dom';
+import {Switch, Route} from 'react-router-dom';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
-
-
-
-const HatsPage = (props) => {
-  console.log(props)
-  return (
-    <div> 
-      <h1>HatsPage PAGE, {props.match.params.topicId}</h1>
-    </div>
-  )};
+import Header from './components/header/header.component'
 
 
 function App() {
   return (
     <div>
+      <Header/>
       <Switch>
         <Route exact path='/' component={HomePage}/>
-        <Route exact path='/shop' component={ShopPage}/>
+        <Route path='/shop' component={ShopPage}/>
       </Switch>
 
     </div>
