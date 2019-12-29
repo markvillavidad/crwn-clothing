219ab6fd2b8c4ee40bb0a8f21b940a6c688e3175
Mark Vidad <markvillavidad@yahoo.com>
2019-12-29 15:01:30 -0500

    Added firestore to save user when logged in thru google

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -15,7 +15,36 @@ const config = {
     measurementId: "G-Y7TBS6Q99Z"
   };
 
-  firebase.initializeApp(config);
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+    if(!userAuth) return ;
+    console.log(userAuth)
+    const userRef = await firestore.doc(`users/${userAuth.uid}`);
+
+    const snapShot = await userRef.get();
+
+    console.log(snapShot);
+
+    if(!snapShot.exists){
+        const {displayName, email} = userAuth;
+        const createdAt = new Date();
+
+        try {
+            await userRef.set({
+                displayName,
+                email,
+                createdAt,
+                ...additionalData
+            })
+        } catch (error) {
+            console.log('error creating user ', error.message);
+
+        }
+    }
+
+    return userRef;
+};
+
+firebase.initializeApp(config);
 
 export const auth = firebase.auth();
 
