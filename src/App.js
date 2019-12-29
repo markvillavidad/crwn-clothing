b3071d1271c47dc6aa03a11e1017e63080fbb16f
Mark Vidad <markvillavidad@yahoo.com>
2019-12-28 23:11:26 -0500

    Added sign-in pages and components

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import {Switch, Route} from 'react-router-dom';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import SignAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component'
 import Header from './components/header/header.component'
 
 
@@ -14,6 +15,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={HomePage}/>
         <Route path='/shop' component={ShopPage}/>
+        <Route path='/signin' component={SignAndSignUpPage}/>
       </Switch>
 
     </div>
