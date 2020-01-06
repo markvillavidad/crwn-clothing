e183b7a39db1e160b9230fdee44322d2f070100f
Mark Vidad <markvillavidad@yahoo.com>
2020-01-06 17:51:47 -0500

    Implemented memoized selectors on cart components

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -4,6 +4,7 @@ import { connect }  from 'react-redux';
 import './cart-dropdown.style.scss';
 import CustomButton from '../custom-button/custom-button.component';
 import CartItem from '../cart-item/cart-item.component';
+import {selectCartItems} from '../../redux/cart/cart.selector'
 
 const CartDropdown = ({cartItems}) => (
     <div className='cart-dropdown'>
@@ -18,8 +19,8 @@ const CartDropdown = ({cartItems}) => (
     </div>
 );
 
-const mapStateToProps = ({ cart: {cartItems} }) => ({
-    cartItems
+const mapStateToProps = (state) => ({
+    cartItems: selectCartItems(state)
 })
 
 export default connect(mapStateToProps)(CartDropdown);
