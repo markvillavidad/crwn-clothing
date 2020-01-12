d366864f8fc3c50aa255af8d54a924ffb402facf
Mark Vidad <markvillavidad@yahoo.com>
2020-01-12 11:14:31 -0500

    Added base checkout page

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import Header from './components/header/header.component'
 import {auth, createUserProfileDocument} from './firebase/firebase.utils'
 import {setCurrentUser} from './redux/user/user.action';
 import {selectCurrentUser} from './redux/user/user.selector'
+import CheckoutPage from './pages/checkout/checkout.component';
 
 
 class App extends React.Component {
@@ -47,6 +48,7 @@ class App extends React.Component {
         <Switch>
           <Route exact path='/' component={HomePage}/>
           <Route path='/shop' component={ShopPage}/>
+          <Route exact path='/checkout' component={CheckoutPage}/>
           <Route 
             exact 
             path='/signin' 
