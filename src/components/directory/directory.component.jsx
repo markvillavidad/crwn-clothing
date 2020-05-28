c0c55fb96369e0597b2ccbb1e917d1270fdfa049
Mark Vidad <mvidad@pelmorex.com>
2020-05-27 23:36:06 -0400

    Section 10

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,62 +1,23 @@
 import React from 'react';
 import MenuItem from '../menu-item/menu-item.component';
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+
+import { selectDirectorySections } from '../../redux/directory/directory.selectors'
+
 import './directory.style.scss';
 
-class Directory extends React.Component {
-    constructor(){
-        super();
+const  Directory = ({ sections }) =>  (
+  <div className='directory-menu'>
+    {sections.map(({id, ...otherProps}) => (
+      <MenuItem key={id} {...otherProps}/>
+    ))}
+  </div>
+);
 
-        this.state = {
-            sections: [
-                {
-                  title: 'hats',
-                  imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
-                  id: 1,
-                  linkUrl: 'shop/hats'
-                },
-                {
-                  title: 'jackets',
-                  imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
-                  id: 2,
-                  linkUrl: 'shop/jackets'
-                },
-                {
-                  title: 'sneakers',
-                  imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
-                  id: 3,
-                  linkUrl: 'shop/sneakers'
-                },
-                {
-                  title: 'womens',
-                  imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
-                  id: 4,
-                  linkUrl: 'shop/womens',
-                  size: 'large'
-                },
-                {
-                  title: 'mens',
-                  imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
-                  id: 5,
-                  linkUrl: 'shop/mens',
-                  size: 'large'
-                }
-              ]
-        };
-    }
-
-    render() {
-        return (
-            <div className='directory-menu'>
-                {
-                    this.state.sections.map(({id, ...otherProps}) => (
-                        <MenuItem key={id} {...otherProps}/>
-                    ))
-                }
-            </div>
-        )
-    }
+const mapStateToProps = createStructuredSelector({
+  sections: selectDirectorySections
+})
 
 
-}
-
-export default Directory;
+export default  connect(mapStateToProps)(Directory);
